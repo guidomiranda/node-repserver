9eeb75307ca5003c437793f920dfab4a55c7b7bd
guido miranda <gmirce@gmail.com>
2020-09-25 15:40:04 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,10 +10,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // BASE DE DATOS
 // ============================================
 let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe'
-// } else {
-//     urlDB = 'mongodb://strider:strider@cluster0.mteem.gcp.mongodb.net/cafe'
-// }
-urlDB = 'mongodb+srv://strider:strider@cluster0.mteem.gcp.mongodb.net/cafe'
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = process.env.MONGO_URI
+}
+
 process.env.URLDB = urlDB
